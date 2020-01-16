8bf2de7157587b4be983f85b5c8429637c34a099
BenjaminVincent <ben.tom.vincent@gmail.com>
2020-01-16 22:43:42 +0000

    updated with tester for breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,24 +1,35 @@
+
+
+
 const request = require('request');
-const args = process.argv.slice(2);
 
 
-const breedOfCat = args[0].trim();
-const URL = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedOfCat;
+const fetchBreedDescription = (breedName, callBack) => {
 
-request(URL, (error, response, body) => {
-  
-  if (error) {
-    console.log("URL Not Found");
-  }
+  const URL = 'https://api.thecatapi.com/v1/breeds/search?q=' + breedName;
 
-  const data = JSON.parse(body);
-  
-  if (data.length === 0) {
-    console.log("breed does not exist");
-  } else {
-    console.log("DESCRIPTION: ", data[0].description);
-  }
+  request(URL, (error, response, body) => {
+    if (error) {
+      callBack(error, null);
+    }
 
-});
+    const data = JSON.parse(body);
+    
+    if (data.length === 0) {
+      callBack(error, null);
+    } else {
+      callBack(null, data[0].description.trim());
+    }
+
+  });
+
+};
+
+
+
+
+module.exports = {
+  fetchBreedDescription
+};
 
 
---
+++ index.js
@@ -0,0 +1,15 @@
+const { fetchBreedDescription } = require('./breedFetcher');
+
+const breedName = process.argv[2];
+
+
+// fetchBreedDescription(breedName, (error, description) => {
+//   if (error) {
+//     console.log("error fetch details ", error);
+//   } else {
+//     console.log(description);
+//   }
+// });
+
+
+
